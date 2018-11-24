6b23dc5818abec77e2f5ad5a42e122d94829dc6a
aangairbender <jetixranger@ya.ru>
2018-11-24 22:00:25 +0200

    ServiceList component add

---
+++ components/ServiceList/ServiceList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { FlatList} from 'react-native'
+import PropTypes from 'prop-types'
+import ServiceListItem from './ServiceListItem'
+
+class ServiceList extends Component {
+	render() {
+		return (
+			<FlatList
+				data={this.props.items}
+				renderItem={({item}) => <ServiceListItem data={item}/>}/>
+		);
+	}
+}
+
+ServiceList.propTypes = {
+	items: PropTypes.array.isRequired
+}
+
+export default ServiceList
