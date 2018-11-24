c58a5d6c66c16ebea6f046d55c5f105397a35b6f
Mykhailo Silaiev <silaievmov@gmail.com>
2018-11-24 20:18:09 +0200

    Microservices added

---
+++ microservices/gateway.js
@@ -0,0 +1,30 @@
+export const GATEWAY_ENDPOINT = 'http://localhost:8762/api';
+
+export function get(path) {
+    return GATEWAY_ENDPOINT + path;
+}
+
+function servicePath(rootPath, pathsGen) {
+    let entry = (path = '') => rootPath + path;
+    let paths = pathsGen(entry)[0];
+    Object.assign(entry, paths);
+    return entry;
+}
+
+export const paths = {
+    auth: 'http://localhost:8080/auth',
+    profiles: servicePath('http://35.244.240.101/profiles', get => [{
+        profile: (id) => get('/' + id),
+        update: get('/update'),
+        search: get('/search'),
+    }]),
+    services: servicePath('http://35.244.186.40/services', get => [{
+        all: get('/all'),
+        service: get('/id'),
+        user: get('/user'),
+        search: get('/intext'),
+    }]),
+    chat: servicePath('http://35.244.172.73', get => [{
+        allComments: get('/comments/aquire/all'),
+    }]),
+};
